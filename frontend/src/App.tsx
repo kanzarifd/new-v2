2ce7d1d153e2c5342c46770289f57d5238e258da
kanzarifd <fedy22ka@gmail.com>
2025-04-25 17:24:40 +0100

    Revert "Feature: Implemented Forgot Password page and route. Users can now request password reset instructions from the login page."
    This reverts commit 8eb74d2603ee873a927cb92c36fe838a92d50aac.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -12,7 +12,6 @@ import AdminDashboard from './pages/AdminDashboard';
 import UserDashboard from './pages/UserDashboard';
 import ResetPassword from './pages/ResetPassword';
 import AgentDashboard from './pages/AgentDashboard';
-import ForgotPassword from './pages/ForgotPassword';
 
 function App() {
   return (
@@ -26,7 +25,6 @@ function App() {
               <Route path="/login" element={<Login />} />
               <Route path="/register" element={<Register />} />
               <Route path="/reset-password" element={<ResetPassword />} />
-              <Route path="/forgot-password" element={<ForgotPassword />} />
 
               {/* Protected Routes */}
               <Route
--- frontend/src/routes/forgotPasswordRoute.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-const ForgotPassword = React.lazy(() => import('../pages/ForgotPassword'));
-
-const forgotPasswordRoute = {
-  path: '/forgot-password',
-  element: <ForgotPassword />,
-};
-
-export default forgotPasswordRoute;
